e3348180cd598dfafdba766d97b99e51453ad162
dkoppad <dkoppad@pdx.edu>
2019-04-29 00:39:38 -0700

    updated transition, Gesture, next & cheat button

---
+++ app/src/main/java/edu/pdx/www/curiouskitty/Question.java
@@ -0,0 +1,28 @@
+package edu.pdx.www.curiouskitty;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+
+    int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
